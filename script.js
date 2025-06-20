56edc028aaf6ee72d22bbd94703438b8496fa1b9
isaiasjoako <100220037+isaiasjoako@users.noreply.github.com>
2025-06-20 14:40:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js");
 }
+function calcular() {
+  const p1 = parseFloat(document.getElementById("p1").value);
+  const p2 = parseFloat(document.getElementById("p2").value);
+  const p3 = parseFloat(document.getElementById("p3").value);
+  const vc = parseFloat(document.getElementById("vc").value);
+  const a = parseFloat(document.getElementById("aumento").value);
+
+  const promedio = (p1 + p2 + p3) / 3;
+  const vi = (promedio * vc) / 10000;
+  const resultado = vi / (1 - a);
+
+  document.getElementById("resultado").textContent = resultado.toFixed(2);
+}
