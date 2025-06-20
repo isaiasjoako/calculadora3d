a4d596567aa192dafb184ea212be033050f8bf20
isaiasjoako <100220037+isaiasjoako@users.noreply.github.com>
2025-06-19 23:28:11 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("mi-cache-v1").then((cache) =>
+      cache.addAll(["/", "/index.html", "/style.css", "/script.js", "/icon-192.png"])
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((respuesta) => respuesta || fetch(e.request))
+  );
+});
